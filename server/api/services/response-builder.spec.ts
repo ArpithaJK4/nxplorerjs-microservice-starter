5c08d98abeeaebd71a5d01eafc10b102aae7bd03
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2025-07-08 22:23:58 +0530

    Update response-builder.spec.ts

--- server/api/services/response-builder.spec.ts
+++ server/api/services/response-builder.spec.ts
@@ -2,24 +2,24 @@ import { ErrorResponseBuilder } from './response-builder';
 import { HttpStatus } from './http-status-codes';
 import '../../common/env';
 
-describe('Error Response Builder Tests', () => {
-  it('can prepare and verify response builder', () => {
-    const errorResponse: any = {
+describe('Error Response Builder', () => {
+  it('should correctly build an error response object', () => {
+    const expectedResponse = {
       title: 'Test Title',
       status: HttpStatus.NOT_FOUND,
       detail: 'Dummy Stack Trace',
       message: 'Error Message',
-      source: 'Error Source'
+      source: 'Error Source',
     };
 
-    const resp = new ErrorResponseBuilder()
-      .setTitle('Test Title')
-      .setStatus(HttpStatus.NOT_FOUND)
-      .setDetail('Dummy Stack Trace')
-      .setMessage('Error Message')
-      .setSource('Error Source')
+    const builtResponse = new ErrorResponseBuilder()
+      .setTitle(expectedResponse.title)
+      .setStatus(expectedResponse.status)
+      .setDetail(expectedResponse.detail)
+      .setMessage(expectedResponse.message)
+      .setSource(expectedResponse.source)
       .build();
 
-    expect(resp).toEqual(errorResponse);
+    expect(builtResponse).toEqual(expectedResponse);
   });
 });
